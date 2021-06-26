7581893a9a27216a6ce5fe87f8d5672a5f35786a
David Isaac Alvarez Garcia <alchemistcodepython@gmail.com>
2021-06-25 23:43:57 -0500

    Saving the label in an array to read more easy and make the arithmetic operation

--- Scripts/Calculadora.gd
+++ Scripts/Calculadora.gd
@@ -1,6 +1,7 @@
 extends Control
 
 # Variables
+var temp:float;
 onready var display:Label = $MarginContainer/VBoxContainer/Panel/Label;
 # Constants
 enum NUMBERS{zero,one,two,three,fourth,five,six,seven,eight,nine};
@@ -9,21 +10,52 @@ var operators:Dictionary = {'add':'+','sub':'-','mul':'*','div':'/'};
 # Here I gonna save the numbers and the operators to make 
 var numbers:Array;
 var alm_operators:Array;
+var aritmetic_operation:Array;
 # Where i gonna content the numbers
 var num_1:float;
 var num_2:float;
-
 
 # Reset the text 'Probando' instead is empty
 func _ready():
 	display.text = "";
 
 func _on_Equal_pressed():
-	pass
-	# mi idea almacenar los valores en un array y otro array los operadores
+	save_it();
+
+func save_it() -> void:
+	# local variables temp number and lenght of the label
+	var temp:String;
+	var lenght:int = len(display.text);
+	# loop through the label
+	for element in range(lenght):
+		# check if are equal to numbers
+		if display.text[element] in NUMBERS.values() as String:
+			# concatenated the numbers in one block
+			temp += display.text[element];
+			# if the lenght of the label it's out of the index
+			if element+1 == lenght:
+				# save it in the array
+				aritmetic_operation.append(temp as float)
+		# if exist an operator after a number
+		elif display.text[element] in operators.values():
+			# save first the number
+			aritmetic_operation.append(temp as float)
+			temp = ''# reset the number
+			# save the operator
+			aritmetic_operation.append(display.text[element])
+#	print("Valores: ",aritmetic_operation)
+	
+func make_operation(value:String, numbers:Array, operators:Array) -> void:
+	if '*' in operators or '/' in operators:
+		pass
+	else:
+		pass
+
 func _on_Erase_pressed():
 	# reset all the text
 	display.text = ""
+	# reset the arrays
+	aritmetic_operation.clear()
 
 func _on_Delete_pressed():
 	# get the size of the label
@@ -31,6 +63,7 @@ func _on_Delete_pressed():
 	# this avoid that len comes less than 0 i.e. -1
 	if get_size_display >= 0:
 		display.text[get_size_display] = '';
+		
 # Aritmetic operators
 func _math_operation(operation):
 	match operation:
@@ -57,42 +90,43 @@ func _found_operator(text:String, operator:String) -> bool:
 		if element == operator:
 			return true;
 	return false;
-	
+
 # Aritmetic functions
 func _on_Add_pressed():
 	var size_display: int = len(display.text);
 	if display.text != '' and !_found_operator(display.text,operators.add):
-		num_1 = display.text as float
 		display.text += operators.add
-		print("Showing the complete value: ",num_1, " and the text: ",display.text);
 	elif _found_operator(display.text,operators.add):
-		# aqui esta un error, verifica
 		if display.text[size_display - 1] != operators.add:
 			display.text += operators.add
 
 func _on_Substraction_pressed():
-	pass
-#	operation = "subtract";
-#	result.text = result.text + '-';
+	var size_display: int = len(display.text);
+	if display.text != '' and !_found_operator(display.text,operators.sub):
+		display.text += operators.sub
+	elif _found_operator(display.text,operators.sub):
+		if display.text[size_display - 1] != operators.sub:
+			display.text += operators.sub
 
 func _on_Divide_pressed():
-	pass
-#	operation = "divide";
-#	result.text = result.text + '/';
+	var size_display: int = len(display.text);
+	if display.text != '' and !_found_operator(display.text,operators.div):
+		display.text += operators.div
+	elif _found_operator(display.text,operators.div):
+		if display.text[size_display - 1] != operators.div:
+			display.text += operators.div
 
 func _on_Multiply_pressed():
-	pass
-#	operation = "multiply";
-#	result.text = result.text + 'X';
+	var size_display: int = len(display.text);
+	if display.text != '' and !_found_operator(display.text,operators.mul):
+		display.text += operators.mul
+	elif _found_operator(display.text,operators.mul):
+		if display.text[size_display - 1] != operators.mul:
+			display.text += operators.mul
 
 # set of numbers
 func _on_7_pressed():
 	display.text += str(NUMBERS.seven);
-#	if num_1 == null:
-#		num_1 = numbers.seven
-#	elif num_2 == null:
-#		pass
-#	result.text = result.text + str(numbers.seven);
 
 func _on_8_pressed():
 	display.text += str(NUMBERS.eight);
@@ -120,4 +154,3 @@ func _on_3_pressed():
 
 func _on_0_pressed():
 	display.text += str(NUMBERS.zero);
-
